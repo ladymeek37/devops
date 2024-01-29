1937b52bf420005df81351a7cdd694854f486aea
Abdelnasser Fried <abdenacer@cureety.com>
2024-01-29 22:13:18 +0100

    wip

--- init.sh
+++ init.sh
@@ -2,10 +2,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    SELECT 'CREATE DATABASE crlw_database' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'crlw_database')\gexec
-EOSQL
-
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    SELECT 'CREATE DATABASE mrs_database' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'mrs_database')\gexec
+	CREATE DATABASE crlw_database;
+    CREATE DATABASE mrs_database;
 EOSQL
